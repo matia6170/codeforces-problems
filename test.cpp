4654cda6a1d743e6e8efe6bd7823f7410d0631ae
matia6170 <matia6170@gmail.com>
2023-09-09 15:52:23 -0500

    merge

--- test.cpp
+++ test.cpp
@@ -1,7 +1,39 @@
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 
+using namespace std;
 
+typedef long long LL;
+
+int main()
+
+{
+    int t;
+
+    cin >> t;
+
+    while (t--) {
+        LL hc, dc, hm, dm, k, w, a;
+
+        cin >> hc >> dc;
+
+        cin >> hm >> dm;
+
+        cin >> k >> w >> a;
+
+        int i = 0;
+
+        for (i = 0; i <= k; i++) {
+            LL midhc = hc + i * a;
+
+            LL middc = dc + (k - i) * w;
+
+            LL col1 = midhc / dm;
+
+            if (midhc % dm != 0) col1++;
+
+            LL col2 = hm / middc;
+
+            if (hm % middc) col2++;
 
             if (col1 >= col2) break;
         }
